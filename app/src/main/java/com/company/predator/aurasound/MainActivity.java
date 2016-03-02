23a016b1a04f44f7be917fa7762db2a988b24e09
Predator <sergey.predatorzzz@gmail.com>
2016-03-02 17:24:50 +0200

    Added first TestSound (Rain), and one onClick method.

--- app/src/main/java/com/company/predator/aurasound/MainActivity.java
+++ app/src/main/java/com/company/predator/aurasound/MainActivity.java
@@ -1,15 +1,23 @@
 package com.company.predator.aurasound;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mySound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mySound = MediaPlayer.create(this, R.raw.rain);
+    }
+
+    public void playSound(View view){
+        mySound.start();
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
